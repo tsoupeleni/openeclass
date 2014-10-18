b798f4a8b7ba303ceb3b91c69411ef00da408749
root <root@user>
2014-10-18 15:13:58 +0300

    step 1

---
+++ config/config.php
@@ -0,0 +1,10 @@
+<?php
+/* ========================================================
+ * Open eClass 3.0 configuration file
+ * Created by install on 2014-10-17 20:31
+ * ======================================================== */
+
+$mysqlServer = 'localhost';
+$mysqlUser = 'openeclassuser';
+$mysqlPassword = 'user25';
+$mysqlMainDb = 'eclass';
